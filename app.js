5f59d82b79b6443193962c8ac48e600cd53ef67b
choko <choko1357@icloud.comgit config --global user.email choko1357@icloud.comgit config --global core.editor vimgit config --global user.email choko1357@icloud.com>
2020-04-27 05:06:01 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 'use strict';
 function fib(n) {
   if (n === 0) {
@@ -13,4 +14,35 @@ const length = 40;
 for (let i = 0; i <= length; i++) {
   console.log(fib(i));
 }
-// node app.js 実行
+// $ node app.js
+*/
+
+/**
+ * 実は、上記は処理に時間がかかっている
+ * アルゴリズムの改良
+ * 連想配列を使う
+ * 連想配列：数字だけでなく、文字列も添字にして使える
+ * 連想配列の添字を(key)という
+ * ES6で利用できるMapを使ってみる
+ */
+
+ 'use strict';
+ // 連想配列,memo
+ const memo = new Map();
+ memo.set(0, 0);
+ memo.set(1, 1);
+ // 
+ function fib(s) {
+   if (memo.has(s)) { // has 特定のkeyに対する要素がMapオブジェクトに存在していたらtrueを返す
+     return memo.get(s);
+   }
+   const value = fib(s - 1) + fib(s -2);
+   memo.set(s, value);
+   return value;
+ }
+ const length2 = 40;
+for (let g = 0; g <= length2; g++) {
+  console.log(fib(g));
+}
+// $ node app.js 
+// $ 結果,爆速
