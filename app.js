8d83c9514fb56416c2f3c5340708bea0c0cba52f
choko <choko1357@icloud.comgit config --global user.email choko1357@icloud.comgit config --global core.editor vimgit config --global user.email choko1357@icloud.com>
2020-04-27 04:07:49 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 'use strict';
+function fib(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  }
+  return fib(n - 1) + fib(n - 2); // 再帰：自分の関数を自分で呼び出している
+}
+
+// 40回ループ
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
+}
+// node app.js 実行
